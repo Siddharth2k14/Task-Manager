98d2d0ccdddbe6815755dee130df466b8d077e03
Siddharth Singh <sid.webd.singh.04@gmail.com>
2026-03-10 02:15:17 +0530

    Adding the updated files of backend 2

--- backend/Controllers/User Controller/registerUser.controller.js
+++ backend/Controllers/User Controller/registerUser.controller.js
@@ -7,27 +7,32 @@ export const registerUser = async (req, res) => {
         const { name, email, password, confirmPassword } = req.body;
 
         if (!name || !email || !password || !confirmPassword) {
-            console.error("All fields are required");
             return res.status(400).json({ message: "All fields are required" });
         }
 
-        const existingUser = await userModel.findOne({ name });
+        if (password !== confirmPassword) {
+            return res.status(400).json({ message: "Passwords do not match" });
+        }
+
+        const existingUser = await userModel.findOne({ email });
+
         if (existingUser) {
             return res.status(400).json({ message: "User already exists" });
         }
 
         const hashedPassword = await bcrypt.hash(password, 10);
-        const token = jwt.sign({
-            name: name
-        },
+
+        const token = jwt.sign(
+            { email },
             process.env.JWT_SECRET,
             { expiresIn: "1h" }
         );
 
         const user = new userModel({
             name,
+            email,
             password: hashedPassword,
-            token: token
+            token
         });
 
         await user.save();
@@ -35,10 +40,11 @@ export const registerUser = async (req, res) => {
         res.status(201).json({
             message: "User registered successfully",
             userId: user._id,
-            token: token
+            token
         });
+
     } catch (error) {
         console.error("Error registering user:", error);
         res.status(500).json({ message: "Error registering user" });
     }
-}
+};
--- backend/Models/User Schema/user.model.js
+++ backend/Models/User Schema/user.model.js
@@ -1,16 +1,22 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+{
     name: {
+        type: String,
+        required: true
+    },
+    email: {
         type: String,
         required: true,
         unique: true
     },
     password: {
-        type: Number,
-        required: true,
-        unique: true
+        type: String,
+        required: true
     }
-});
+},
+{ timestamps: true }
+);
 
 export default mongoose.model("User", userSchema);
