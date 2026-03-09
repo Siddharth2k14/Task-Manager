5a47492f0275456f712f37712a22100e18ba9a32
Siddharth Singh <sid.webd.singh.04@gmail.com>
2026-03-09 17:01:20 +0530

    Adding the updated files

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,12 +2,16 @@ import express from "express"
 import cors from "cors";
 import userRoutes from "../Routes/User Routes/user.routes.js";
 import taskRoutes from "../Routes/Task Routes/task.routes.js";
+import { connectUserDB } from "../DB/User Database/user.db.js";
+import { connectTaskDB } from "../DB/Task Database/task.db.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors())
+connectUserDB()
+connectTaskDB()
 app.use("/api/users", userRoutes);
 app.use("/api/tasks", taskRoutes);
 
