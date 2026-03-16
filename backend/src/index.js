7f5cb864d83c79d3bfc580fd3107d7143f93123c
Siddharth Singh <sid.webd.singh.04@gmail.com>
2026-03-16 21:46:12 +0530

    Adding the updated file in the backend folder

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(
-    'http://localhost:5173/'
+    'http://localhost:5173'
 ));
 connectUserDB();
 connectTaskDB();
