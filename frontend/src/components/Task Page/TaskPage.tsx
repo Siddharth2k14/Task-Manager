ae18e693d0075d350dc0a7d47d9809381eb8b494
Siddharth Singh <sid.webd.singh.04@gmail.com>
2026-03-17 00:49:24 +0530

    Adding the updated file of the frontend under the Styles and Component folder

--- frontend/src/components/Task Page/TaskPage.tsx
+++ frontend/src/components/Task Page/TaskPage.tsx
@@ -3,15 +3,18 @@ import { CalendarView } from "../CalendarView";
 import { CompleteTasks } from "../Completed Tasks/CompleteTasks";
 import { TaskForm } from "../TaskForm";
 import { TaskList } from "../TaskList";
-import "../../Styles/Task.style.css";
 import { useDispatch, useSelector } from "react-redux";
 import type { RootState } from "../../redux/store";
 import { addTask, deleteTask, toggleTask } from "../../redux/taskSlice";
+import { useNavigate } from "react-router-dom";
+import "../../Styles/Task.style.css";
+import "../../Styles/Logout.style.css";
 
 export const TaskPage = () => {
 
     const tasks = useSelector((state: RootState) => state.tasks);
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     const handleAddTask = (task: Task) => {
         dispatch(addTask(task));
@@ -25,12 +28,26 @@ export const TaskPage = () => {
         dispatch(toggleTask(index));
     };
 
+    const handleLogout = () => {
+        // remove token
+        localStorage.removeItem("token");
+
+        // redirect to login page
+        navigate("/login");
+    };
+
     const completedTasks = tasks.filter(task => task.status === "completed");
 
     return (
         <div className="app">
 
-            <h1>Task Manager {tasks.length}</h1>
+            <div style={{display:"flex", justifyContent:"space-between", alignItems:"center"}}>
+                <h1>Task Manager {tasks.length}</h1>
+
+                <button onClick={handleLogout} className="logout-btn">
+                    Logout
+                </button>
+            </div>
 
             <TaskForm addTask={handleAddTask} />
 
