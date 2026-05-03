08f428765a4c9cbf62120f9e5a9e7a886ec90015
Siddharth Singh <sid.webd.singh.04@gmail.com>
2026-05-03 23:01:11 +0530

    Adding the files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,27 +5,13 @@ import { TaskPage } from './components/Task Page/TaskPage';
 import Home from './components/Home Page/Home';
 
 function App() {
-  
-  
-
   return (
-    // <div className="app">
-    //   <h1>Task Manager</h1>
-    //   <TaskForm addTask={addTask} />
-    //   <TaskList tasks={tasks} deleteTask={deleteTask} />
-    //   <CalendarView tasks={tasks} />
-    // </div>
-    <>
-      {/* <Login /> */}
-      {/* <Register /> */}
-      {/* <TaskPage /> */}
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/tasks" element={<TaskPage />} />
       </Routes>
-    </>
   )
 }
 
